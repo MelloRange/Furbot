c3581262a4b23229e489a788d834eedd938084a9
melliehan <71661154+MelloRange@users.noreply.github.com>
2023-02-03 18:02:13 -0500

    Updated Database
    Added Primary Keys to variables.

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Servers(
-    server_id int NOT NULL UNIQUE,
+    server_id int PRIMARY KEY,
     server_name text
 );
 
 CREATE TABLE IF NOT EXISTS Users(
-    user_id int NOT NULL UNIQUE
+    user_id int PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS user_in_server(
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS user_in_server(
     is_in_server bit DEFAULT 1,
     FOREIGN KEY (server_id) REFERENCES Servers(server_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    UNIQUE(server_id, user_id)
+    PRIMARY KEY(server_id, user_id)
 );
 
 CREATE TABLE IF NOT EXISTS Channels(
+    channel_id int PRIMARY KEY,
     server_id int NOT NULL,
-    channel_id int NOT NULL UNIQUE,
     FOREIGN KEY (server_id) REFERENCES Servers(server_id)
 );
